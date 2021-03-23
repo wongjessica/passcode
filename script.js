2ca3ac68f40888b5a6f1e7ede7ede9fe76edfb58
Jessica Wong <39626651+wongjessica@users.noreply.github.com>
2021-03-23 13:33:17 -0400

    finished base passcode functions and html

---
+++ script.js
@@ -0,0 +1,18 @@
+let code1 = document.querySelector('#code1');
+let code2 = document.querySelector('#code2');
+let code3 = document.querySelector('#code3');
+let code4 = document.querySelector('#code4');
+let switchBtn = document.querySelector('input[type="button"]');
+
+function checkForm() {
+  if(form.code1.value === '21' && form.code2.value === '34' && form.code3.value === '55' && form.code4.value === '89') {
+    alert("Congrats!");
+    console.log("congrats");
+    return false;
+  }
+  else {
+    console.log("try again");
+    alert("Error: Wrong Code");
+    return false;
+  }
+}
